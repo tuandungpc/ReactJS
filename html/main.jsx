7e5b9e6054441a4f303917e0e4e8719af3fdad52
Dung Pham <tuandungpc@gmail.com>
2018-12-05 09:37:50 +0700

    First Editon

--- html/main.jsx
+++ html/main.jsx
@@ -1,13 +1,32 @@
 class Signin extends React.Component{
+    constructor(props){
+        super(props);
+        this.signIn = this.signIn.bind(this);
+        this.handleEmailChange = this.handleEmailChange.bind(this);
+        this.handlePasswordChange = this.handlePasswordChange.bind(this);
+        this.state = {
+            email:'',
+            password:''
+        }
+    }
+    signIn(){
+        alert('Email address is ' + this.state.email + 'Password is ' + this.state.password);
+    }
+    handleEmailChange(e){
+        this.setState({email:e.target.value})
+    }
+    handlePasswordChange(e){
+        this.setState({password:e.target.value})
+    }
     render() {
         return (
             <form className="form-signin">
                 <h2 className="">Please sign in</h2>
                 <label for="inputEmail" className="sr-only">Email address</label>
-                <input type="email" id="inputEmail" className="form-control" placeholder="Email address" required autofocus />
+                <input type="email" onChange={this.handleEmailChange} id="inputEmail" className="form-control" placeholder="Email address" required autofocus />
                 <label for="inputPassword" className="sr-only"> Password</label>
-                <input type="password" id="inputPassword" className="form-control" placeholder="Password" required />
-                <button className="btn btn-lg btn-primary btn-block" type="button"> Sign in </button>
+                <input type="password" onChange={this.handlePasswordChange} id="inputPassword" className="form-control" placeholder="Password" required />
+                <button className="btn btn-lg btn-primary btn-block" onClick={this.signIn} type="button"> Sign in </button>
             </form>
         )
     }
